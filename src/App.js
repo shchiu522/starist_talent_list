4a4e3b9697161d2864359fd122845539314adab7
Peter Chiu <sunghimchiu@gmail.com>
2022-11-03 20:28:28 +0800

    Add: content header; header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.scss';
-import startistLogo from './assets/starist_logo1.png';
+import staristLogo from './assets/starist_logo1.png';
+import { Component } from "react";
+
 // const startistLogo = require('./assets/starist_logo1.png');
 
+class Header extends Component {
+  render() {
+    return <header className="App-header">
+      <img src={staristLogo} className="App-logo" alt="logo" />
+    </header>;
+  }
+}
+
+function ContentHeader() {
+  return <div className={"content-header"}>
+    <h2>配音員</h2>
+    <div className={`custom-border-yellow`}></div>
+  </div>;
+}
+
+function MainContent(props) {
+  return (<section className={'body'}>
+    {props.children}
+  </section>);
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={startistLogo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+  return (<div className="App">
+    <Header />
+    <MainContent>
+      <ContentHeader />
+
+    </MainContent>
+  </div>);
 }
 
 export default App;
